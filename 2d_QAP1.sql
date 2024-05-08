5fdd7d02b52a6ebeedad89b606e29c11def7f28e
Brenda Armstrong <137068502+brendaleearmstrong@users.noreply.github.com>
2024-05-08 13:37:04 -0230

    Update 2d_QAP1.sql

--- 2d_QAP1.sql
+++ 2d_QAP1.sql
@@ -1,6 +1,6 @@
 -- Deliverable 2d) Order the results using an ORDER BY clause.
 -- Table: Film 
--- ORDER BY: Using 'ORDER BY' and 'DESC' to group/sort title from A-Z.
+-- ORDER BY: Using 'ORDER BY' and 'ASC' to group/sort title from A-Z.
 
 SELECT title, release_year, length, rating
 FROM film
